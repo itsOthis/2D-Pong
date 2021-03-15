46a26d0cc5ce8af6bb5dbbf3b64429cb43e48128
itsOthis <its.othis@gmail.com>
2021-03-14 22:57:20 -0400

    final update

--- 2D Pong/Assets/Ball.cs
+++ 2D Pong/Assets/Ball.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField]
+    float speed;
+
+    float radius;
+    Vector2 direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = Vector2.one.normalized; // direction is (1,1) normalized
+        radius = transform.localScale.x / 2; // half the width
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        // Bounces off bottom and top
+        if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0) {
+            direction.y = -direction.y; // hits bottom wall
+        }
+        if (transform.position.y > GameManager.topRight.y - radius && direction.y > 0) {
+            direction.y = -direction.y; // hits top wall
+        }
+
+        // Game over
+        if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0) {
+            Debug.Log("Right player wins!");
+
+            Time.timeScale = 0; // freezes time
+            enabled = false; // disables script
+        }
+        if (transform.position.x > GameManager.topRight.x - radius && direction.x > 0) {
+            Debug.Log("Left player wins!");
+
+            Time.timeScale = 0; // freezes time
+            enabled = false; // disables script
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Paddle") {
+            bool isRight = other.GetComponent<Paddle> ().isRight;
         
+            // If hitting right paddle and moving right, flip direction
+            if (isRight && direction.x > 0) {
+                direction.x = -direction.x;
+            }
+
+            // If hitting left paddle and moving left, flip direction
+            if (!isRight && direction.x < 0) {
+                direction.x = -direction.x;
+            }
+        }
     }
 }
--- 2D Pong/Assets/Paddle.cs
+++ 2D Pong/Assets/Paddle.cs
@@ -4,32 +4,65 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField]
+    float speed;
+
+    float height;
+
+    string input;
+    public bool isRight; // can access from ball class
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        height = transform.localScale.y;
     }
 
     public void Init(bool isRightPaddle) {
+
+        isRight = isRightPaddle;
 
         Vector2 pos = Vector2.zero;
 
         if (isRightPaddle) {
             // Place paddle on right side of screen
             pos = new Vector2(GameManager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x; // Move a bit to the left
+
+            input = "PaddleRight";
         }
         else {
             // Place paddle on left side of screen
             pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x; // Move a bit to the right
+
+            input = "PaddleLeft";
         }
 
         // Update this paddle's position
         transform.position = pos;
+
+        transform.name = input;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Now let's move the paddle!
+
+        // GetAxis is a number between -1 and 1 (-1 for down, 1 for up)
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        // Restrict paddle movement
+        // If paddle is too low and continuing to move down, stop
+        if (transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0) {
+            move = 0;
+        }
+        // If paddle is too high and continuing to move up, stop
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0) {
+            move = 0;
+        }
+
+        transform.Translate(move * Vector2.up); // vector [0,1]
     }
 }
